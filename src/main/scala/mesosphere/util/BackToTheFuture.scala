1963b77b3441ee5be49ffb808be1800b7065f5aa
Shingo Omura <everpeace@gmail.com>
2014-04-15 22:09:58 -0700

    future conversion of BackToTheFuture was changed to f.get(timeout, unit) from f.get
    it's because f.get blocks until the computation finished.  However, if f is a future returned from ZookeeperState, f eventually blocks forever when Zookeeper died.  This will also blocks MarathonSchedulerService's shutdown process.

--- src/main/scala/mesosphere/util/BackToTheFuture.scala
+++ src/main/scala/mesosphere/util/BackToTheFuture.scala
@@ -1,15 +1,26 @@
 package mesosphere.util
 
-import scala.concurrent._
+import scala.language.postfixOps
 import scala.language.implicitConversions
+import scala.concurrent._
+import scala.concurrent.duration._
+import java.util.concurrent.TimeUnit
 
 object BackToTheFuture {
 
   import ExecutionContext.Implicits.global
 
-  implicit def FutureToFutureOption[T](f: java.util.concurrent.Future[T]): Future[Option[T]] = {
+  // TODO(everpeace) make it configurable or be declared outside of this object.
+  implicit val _timeout = BackToTheFutureTimeout(2 seconds)
+  case class BackToTheFutureTimeout(d:Duration)
+
+  implicit def FutureToFutureOption[T](f: java.util.concurrent.Future[T])(implicit timeout:BackToTheFutureTimeout): Future[Option[T]] = {
     future {
-      val t = f.get
+      // f.get() blocks until the computation finished.
+      // Particularly, if f is an future instance returned from AbstractState,
+      // f.get could block forever this promise thread when Zookeeper was dead.
+      // and this will also block MarathonSchedulerService's shutdown process.
+      val t = f.get(timeout.d.toNanos, TimeUnit.NANOSECONDS)
       if (t == null) {
         None
       } else {
